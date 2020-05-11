68109fa3330402a13bb63273b93a20d191bc9a49
Mohamed Nuh <mojitsu185@gmail.com>
2020-05-11 13:29:15 -0700

    Added css transitions

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 // Created the necessary const
 const randUserUrl = 'https://randomuser.me/api/';
 const groupNum = "12"
-const randGroupUrl = randUserUrl+`/?nat=au,us,dk,fr,gb&lego&results=${groupNum}`;
+const randGroupUrl = randUserUrl+`/?nat=au,us,dk,fr,gb&results=${groupNum}`;
 const searchContainer = document.querySelector('.search-container');
 const gallery = document.getElementById('gallery');
 
