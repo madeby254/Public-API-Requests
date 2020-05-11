dc1624b09f310762b60a4146fc687721f6d4c364
Mohamed Nuh <mojitsu185@gmail.com>
2020-05-11 12:39:29 -0700

    async

--- app.js
+++ app.js
@@ -2,4 +2,17 @@
 
 // Created and Empty Array for Users
 
-const users = []
+const users = [];
+
+
+// Fecth Functions
+
+async function fetchData(url) {
+    try {
+      let response = await fetch(url);
+      let data = await response.json();
+      return data.results;
+    } catch (error) {
+      console.log('Looks like there was a problem getting data!',error);
+    }
+  }
