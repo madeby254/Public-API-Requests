c583030f338bac2c4bb50b2b0d431efdfc8e7fc1
Mohamed Nuh <mojitsu185@gmail.com>
2020-05-15 23:57:45 -0700

    Phone format

--- app.js
+++ app.js
@@ -7,6 +7,17 @@ const randGroupUrl = randUserUrl+`/?nat=au,us,dk,fr,gb&results=${groupNum}`;
 const searchContainer = document.querySelector('.search-container');
 const gallery = document.getElementById('gallery');
 
+function formatPhoneNumber(phoneNumberString) {
+  var cleaned = ('' + phoneNumberString).replace(/\D/g, '')
+  var match = cleaned.match(/^(1|)?(\d{3})(\d{3})(\d{4})$/)
+  if (match) {
+    var intlCode = (match[1] ? '+1 ' : '')
+    return [intlCode, '(', match[2], ') ', match[3], '-', match[4]].join('')
+  }
+  return null
+}
+formatPhoneNumber('+12345678900') // => "+1 (234) 567-8900"
+formatPhoneNumber('2345678900')   // => "(234) 567-8900"
 
 // Fecth Functions
 
